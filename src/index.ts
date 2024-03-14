fdb277d2ad88959d971d9e83a688edd8f0e28efc
rakesh-paulraj <rakeshpaulraj2004@gmail.com>
2024-03-14 22:33:45 +0530

    some more files

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
-interface User{
-    name:string;
-    age:number;
-    number:number;
-    email:string;
-    password:string;
+const users ={
+    "employee1":{
+        "name":"Rakesh",
+        "age":20,
+    },
+    "employee2":{
+        "name":"Mukesh",
+        "age":28,
+    }
+
 }
-type Updatedprops = Pick<User,"name"|"age">
-
-function sumofAge (user1:Updatedprops,user2:Updatedprops){
-    return user1.age+user2.age;
-}
-
-const age=sumofAge({name:"Rakesh",age:20},{name:"Mukesh",age:28});
-console.log (age);
-
